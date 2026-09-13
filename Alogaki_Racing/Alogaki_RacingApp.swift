a24fc0c0d0a468b73ea575409971ab6bf7eaf3e5
fwntas <zokiassan@gmail.com>
2026-09-13 08:53:57 +0300

    Initial Commit

---
+++ Alogaki_Racing/Alogaki_RacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alogaki_RacingApp.swift
+//  Alogaki_Racing
+//
+//  Created by Xenofon on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct Alogaki_RacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
